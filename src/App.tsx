5be7f1fdf4a34ffff4a44c8c443127d931053526
lourencosalvador <lourencocardoso007@gmail.com>
2024-01-31 12:57:21 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,37 +3,54 @@ import './App.css'
 import { Subject, interval } from 'rxjs'
 
 
-type Grounps = {
-  name: string,
-  notifiq: string
+type typeProduct = {
+  id: number,
+  nome: string
 } 
 
-const notifiqFrases = [
-  'Nova mensagem recebida!',
-  'Atualização disponível. Clique para instalar.',
-  'Você tem 3 novas notificações.'
-]
-
-
 function App() {
-  const [resultNotifiq, setResultNotifiq] = useState('')
-   
 
-  useEffect(() => {
-   const subscription = interval(1000).subscribe(()=> {
-    const indice = Math.floor(Math.random() * notifiqFrases.length)
-    setResultNotifiq(notifiqFrases[indice])
-   })
-    
-   return () => {
-    subscription.unsubscribe()
-   }
-  }, [])
+  const [item, setItem] = useState<typeProduct[]>([
+    { id: 1, nome: 'Camiseta' },
+    { id: 2, nome: 'Calça Jeans' },
+    { id: 3, nome: 'Tênis' },
+    { id: 4, nome: 'Óculos de Sol' },
+    { id: 5, nome: 'Bolsa' },
+    { id: 6, nome: 'Relógio' },
+    { id: 7, nome: 'Boné' },
+    { id: 8, nome: 'Sapato' }
+  ])
+
+  const subject = new Subject()
+
+  const observable  = subject.asObservable()
+
+  const deleteItem = useCallback((id: number, item: string)=> {
+    setItem(prevItem => prevItem.filter(item => item.id !== id));
+    subject.next(item)
+  }, []) 
+
+  
+
+  observable.subscribe({
+     next: (value) => console.log('o mesmo item foi deletado ' + value),
+     error: (err) => console.log('Um erro foi emetido ' + err),
+     complete: () => console.log("O sucess concluido")
+  })
+
   return (
     
     <>
       <div> 
-        <h1>{resultNotifiq}</h1>
+        {
+          item.map((item, index)=> (
+            <>
+             <div id='div' key={index}>
+             <h2>{item.nome}</h2> <button onClick={() => deleteItem(item.id, item.nome)}>deletar</button>
+             </div>
+            </>
+          ))
+        }
       </div>
     </>
   )
